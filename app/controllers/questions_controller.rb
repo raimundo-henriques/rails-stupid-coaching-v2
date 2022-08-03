832b8074f25600fdb86abaa68083eb9968e69a29
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-03 20:10:15 +0100

    TA prep: added pre-written tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,14 @@
 class QuestionsController < ApplicationController
-  def ask
+  def ask; end
+
+  def answer
     @question = params[:question]
+    @answer = if @question.downcase == 'i am going to work'
+                'Great!'
+              elsif @question[-1] == '?'
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
   end
 end
